85cf93a6b490541b3b3cdfafb139e776376780bf
Vullnettt <vullnetv389@gmail.com>
2023-04-12 23:59:45 +0200

    chore: Feign Client for send emails implemented in digital school

--- digitalschool/src/main/java/org/zerogravitysolutions/DigitalSchoolApplication.java
+++ digitalschool/src/main/java/org/zerogravitysolutions/DigitalSchoolApplication.java
@@ -2,8 +2,10 @@ package org.zerogravitysolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DigitalSchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(DigitalSchoolApplication.class, args);
---
+++ digitalschool/src/main/java/org/zerogravitysolutions/client/EmailFeignClient.java
@@ -0,0 +1,21 @@
+package org.zerogravitysolutions.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@FeignClient(value = "email-service", url = "${feign.address.email-service}")
+public interface EmailFeignClient {
+
+    @PostMapping(path = "/send", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    void send(@RequestPart(name = "recipients", required = true) String recipients,
+                                  @RequestPart(name = "subject") String subject,
+                                  @RequestPart(name = "body", required = false) String body,
+                                  @RequestPart(name = "attachments", required = false) MultipartFile[] attachments,
+                                  @RequestPart(name = "templateFile", required = false) MultipartFile templateFile,
+                                  @RequestPart(name = "templateData", required = false) String templateData);
+}
