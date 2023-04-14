c7bfd1007b3031d46b4d2a50be067abb8c6195ee
Vullnettt <vullnetv389@gmail.com>
2023-04-15 01:25:25 +0200

    Add discovery client in digitalschool-service

--- digitalschool/src/main/java/org/zerogravitysolutions/DigitalSchoolApplication.java
+++ digitalschool/src/main/java/org/zerogravitysolutions/DigitalSchoolApplication.java
@@ -2,10 +2,12 @@ package org.zerogravitysolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class DigitalSchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(DigitalSchoolApplication.class, args);
