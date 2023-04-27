3831ea17df2fc0d68afd950ccb0c3139c7a6d8f5
Vullnettt <vullnetv389@gmail.com>
2023-04-27 12:31:31 +0200

    Add dependencies in api-gateway service

---
+++ api-gateway/src/main/java/org/zerogravitysolutions/ApiGatewaySpringApplication.java
@@ -0,0 +1,11 @@
+package org.zerogravitysolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewaySpringApplication.class, args);
+    }
+}
