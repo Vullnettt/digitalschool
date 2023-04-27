d4545655f9e4997df76e724d9a0aa6ec8bb3b694
Vullnettt <vullnetv389@gmail.com>
2023-04-27 13:16:50 +0200

    Add route for training component

--- api-gateway/src/main/java/org/zerogravitysolutions/ApiGatewaySpringApplication.java
+++ api-gateway/src/main/java/org/zerogravitysolutions/ApiGatewaySpringApplication.java
@@ -2,8 +2,10 @@ package org.zerogravitysolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewaySpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewaySpringApplication.class, args);
